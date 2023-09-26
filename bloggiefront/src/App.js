d649e1acbd030a4776fd0847e8f3f64ffa03a213
Shreyas.j <shreyasjadhav1105511@gmail.com>
2023-09-26 14:24:45 +0530

    Component added in src directory.

--- bloggiefront/src/App.js
+++ bloggiefront/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import team from '.component/team';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React and make project
         </a>
       </header>
+      <Route component={team} path="/team" />
     </div>
   );
 }
